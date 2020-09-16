74b72ecc828362b2916ec58ed55743ec7427890e
mmayes3 <mauricelynell@gmail.com>
2020-09-16 08:24:33 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
 class String
-
+  def inlitialize(sentence)
+    @sentence = sentence
+  end
   def sentence?
     self.end_with?(".")
   end
@@ -15,7 +17,11 @@ class String
   end
 
   def count_sentences
-    array = self.split(/[.?!]/).reject {|string| string.empty?}
+    p array = self.split(/[.?!]/).reject {|string| string.empty?}
     array.length
   end
 end
+
+# s = "This, well, is a sentence. This is too!! And so is this, I think? Woo"
+# s = String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo")
+# puts s.count_sentences
